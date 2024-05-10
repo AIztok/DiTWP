13a35c4978ee1b142e7de09bcb99e7a64e533cfa
AIztok <128750281+AIztok@users.noreply.github.com>
2024-05-10 13:03:25 +0200

    Quartz sync: May 10, 2024, 1:03 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,8 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-  ],
-  right: [Component.Graph()],
+  ]
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
